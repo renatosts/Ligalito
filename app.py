19a138d956bb811a0658e9334e46e6ae1e274164
Renato S. Thiago <renatosts@gmail.com>
2023-04-23 17:16:21 -0300

    Ordem das colunas

--- app.py
+++ app.py
@@ -13,35 +13,50 @@ def define_color(val):
     return 'color: %s' % color
 
 
+@st.cache(persist=True)
+def getFile(f):
+    return pd.read_csv(f, sep=';', thousands='.', decimal=',', encoding='Latin-1')
+
 st.set_page_config(
     layout='wide',
     initial_sidebar_state='collapsed',
     page_icon='app.jpg',
     page_title='Ligalito')
 
-#@st.cache(persist=True)
-def getFile(f):
-    return pd.read_csv(f, sep=';', thousands='.', decimal=',', encoding='Latin-1')
-
 
 f = 'https://raw.githubusercontent.com/renatosts/Ligalito/master/cartola.csv'
 #f = 'cartola.csv'
 
 cart = getFile(f)
+
 cart = cart.sort_values(['cart_nome', 'cart_rodada'])
 
 cart['cart_acum'] = round(cart.groupby(['cart_nome'])['cart_pontos'].cumsum(), 2)
 
-df = cart[cart.cart_rodada > 0].pivot(values='cart_pontos', index='cart_nome', columns='cart_rodada')
-df = df.rename(columns={'cart_rodada': 'index'})
+ult_rodada = cart.cart_rodada.max()
 
-df['Total'] = df.sum(axis=1)
+df = cart[cart.cart_rodada > 0].pivot(values='cart_pontos', index='cart_nome', columns='cart_rodada').reset_index()
+
+colunas = [i for i in range(1, ult_rodada + 1)]
+colunas.sort(reverse=True)
+
+df['Total'] = df[colunas].sum(axis=1)
+
 df = df.sort_values(['Total', 'cart_nome'], ascending=[False, True])
+
+df = df[['cart_nome', 'Total'] + colunas]
+
+df = df.set_index('cart_nome')
+
 df_aux = df
 
-df_aux = df_aux.style.format(thousands=".",
-                             decimal = ",",
-                             precision=2)
+df_aux = df_aux.style.format(thousands='.',
+                             decimal = ',',
+                             precision=2).applymap(define_color, subset=['Total'])
+
+
+#st.header('LIGALITO')
+
 st.dataframe(df_aux)
 
 
@@ -59,21 +74,21 @@ for nome in cart.cart_nome.drop_duplicates():
     fig2.add_trace(
         go.Scatter(x=df.cart_rodada, y=df.cart_patr, name=nome))
 
-fig1.update_layout(title='Pontos')
+fig1.update_layout(title='Pontuação')
 fig2.update_layout(title='Patrimônio')
 
 fig1.update_layout(legend=dict(
-    orientation="h",
-    yanchor="bottom",
+    orientation='h',
+    yanchor='bottom',
     y=1.02,
-    xanchor="right",
+    xanchor='right',
     x=1))
 
 fig2.update_layout(legend=dict(
-    orientation="h",
-    yanchor="bottom",
+    orientation='h',
+    yanchor='bottom',
     y=1.02,
-    xanchor="right",
+    xanchor='right',
     x=1))
 
 fig1.update_xaxes(scaleratio = 1)
